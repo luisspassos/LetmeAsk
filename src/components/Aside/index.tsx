bb80214298ffcd8e6dfe3b06959e1db1359c4dad
luis <luis.passos013@gmail.com>
2022-01-19 19:57:19 -0300

    add Aside component and the styled components library

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,13 @@
+import illustrationImg from '../../assets/images/illustration.svg'
+
+import './styles.scss'
+
+export function Aside() {
+  return (
+    <aside>
+      <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+      <strong>Crie salas de Q&amp;A ao-vivo</strong>
+      <p>Tire as dúvidas da sua audiência em tempo-real</p>
+    </aside>
+  )
+}
