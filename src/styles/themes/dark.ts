70755770dab30ecd4f6d7257bcdb6d747578e941
luis <luis.passos013@gmail.com>
2022-01-21 12:28:51 -0300

    add dark.ts and light.ts

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -1,3 +1,24 @@
 export const dark = {
-  
+  title: 'dark',
+
+  colors: {
+    background:'#121212',
+
+    color_primary: '#835afd',
+    color_secondary: '#ea4335',
+    color_tertiary: '#E559F9',
+    color_quaternary: '#e2e2e2',
+    color_quinary: '#fefefe',
+    color_senary: '#DBDCDD',
+    color_septenary: '#F4F0FF',
+
+    color_switcher_icon: '#F2B705',
+    color_switcher_background: '#555',
+
+    // finished here
+
+    text_primary: '#a8a8b3',
+    text_secondary: '#737380',
+    text_tertiary: '#29292E',
+  }
 }
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -9,6 +9,8 @@ export const light = {
     color_tertiary: '#E559F9',
     color_quaternary: '#e2e2e2',
     color_quinary: '#fefefe',
+    color_senary: '#DBDCDD',
+    color_septenary: '#F4F0FF',
 
     color_switcher_icon: '#F2B705',
     color_switcher_background: '#555',
