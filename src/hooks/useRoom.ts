8b95880eab315245955dd51469d44abf11431def
Luís Passos <luis.passos013@gmail.com>
2022-01-23 12:50:44 -0300

    I separated the function to get the title

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -33,42 +33,47 @@ export type Question = {
 type RoomData = {
   title: string;
   authorId: string;
-} | undefined
+}
 
 export function useRoom(roomId: string) {
   const { user } = useAuth()
   const [questions, setQuestions] = useState<Question[]>([])
   const [title, setTitle] = useState('')
 
-  useEffect(() => {
-
+  async function getData() {
     const roomRef = database.collection('rooms').doc(roomId)
 
-    const unsubscribe = roomRef.onSnapshot(room => {
-      const databaseRoom = room.data<RoomData>()
-      // ver swr-firebase asjdijasdijoasjiodijasjdi
-      console.log(databaseRoom)
-      // const firebaseQuestions: FirebaseQuestions = databaseRoom?.questions ?? {};
-      //     const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-      //       return {
-      //         id: key,
-      //         content: value.content,
-      //         author: value.author,
-      //         isHighlighted: value.isHighlighted,
-      //         isAnswered: value.isAnswered,
-      //         likeCount: Object.values(value.likes ?? {}).length,
-      //         likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
-      //       }
-      //     })
+    const roomData = (await roomRef.get()).data() as RoomData
 
-      setTitle(databaseRoom.title)
-      //     setQuestions(parsedQuestions)
-      //   })
+    setTitle(roomData.title)
+  }
 
-      return () => {
-        unsubscribe()
-      }
-    })
+  useEffect(() => {
+
+    getData()
+
+  //   const unsubscribe = roomRef.onSnapshot(room => {
+  //     const databaseRoom = room.data() as RoomData
+  //     // const firebaseQuestions: FirebaseQuestions = databaseRoom?.questions ?? {};
+  //     //     const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+  //     //       return {
+  //     //         id: key,
+  //     //         content: value.content,
+  //     //         author: value.author,
+  //     //         isHighlighted: value.isHighlighted,
+  //     //         isAnswered: value.isAnswered,
+  //     //         likeCount: Object.values(value.likes ?? {}).length,
+  //     //         likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
+  //     //       }
+  //     //     })
+
+  //     //     setQuestions(parsedQuestions)
+  //     //   })
+
+  //     return () => {
+  //       unsubscribe()
+  //     }
+  //   })
   }, [roomId, user?.id])
 
   return { questions, title }
