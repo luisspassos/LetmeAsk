6845667e7de3edca5afa3f7912628463292d8c45
luis <luis.passos013@gmail.com>
2022-01-27 16:29:27 -0300

    add eslint and format app.tsx and index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
-import { Home } from './pages/Home'
-import { NewRoom } from "./pages/NewRoom";
-import { Room } from './pages/Room';
+import { useTheme } from './hooks/useTheme';
 import { AdminRoom } from './pages/AdminRoom';
-
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
-
-import { useTheme } from './hooks/useTheme'
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
+import { GlobalStyle } from './styles/global';
 
 function App() {
-  const { state, themes } = useTheme()
+  const { state, themes } = useTheme();
 
   return (
     <BrowserRouter>
-      <ThemeProvider theme={state.theme === 'dark' ? themes.dark : themes.light}>
+      <ThemeProvider
+        theme={state.theme === 'dark' ? themes.dark : themes.light}
+      >
         <GlobalStyle />
         <Routes>
           <Route path="/" element={<Home />} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import { ColorThemeProvider } from './contexts/ColorThemeContext'
-import { AuthContextProvider } from './contexts/AuthContext'
+import App from './App';
+import { AuthContextProvider } from './contexts/AuthContext';
+import { ColorThemeProvider } from './contexts/ColorThemeContext';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './services/firebase';
 
